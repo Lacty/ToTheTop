0de880997c0a6475546e4d59dbba0463eac8e0de
Lacty <akira206@gmail.com>
2016-12-03 02:19:38 +0900

    Brickを追加

--- src/actors/brick.cpp
+++ src/actors/brick.cpp
@@ -1,9 +1,34 @@
-//
-//  brick.cpp
-//  ToTheTop
-//
-//  Created by Yanai on 2016/12/03.
-//
-//
 
-#include "brick.hpp"
+#include "brick.h"
+
+
+Brick::Brick() {
+  size_.set(20, 20);
+}
+
+Brick::Brick(const ofVec3f& pos, const ofVec3f& size, bool active) :
+active_(active) {
+  pos_  = pos;
+  size_ = size;
+}
+
+
+void Brick::setup() {}
+
+void Brick::update(float deltaTime) {
+  if (!active_) { return; }
+  pos_.y -= deltaTime * 10.0f;
+}
+
+void Brick::draw() {
+  if (active_) {
+    color_ = ofColor(140, 140, 250);
+  } else {
+    color_ = ofColor(250, 140, 140);
+  }
+  
+  ofSetColor(color_);
+  ofDrawRectangle(pos_, size_.x, size_.y);
+}
+
+bool& Brick::active() { return active_; }
--- src/actors/brick.h
+++ src/actors/brick.h
@@ -1,2 +1,20 @@
 
 #pragma once
+#include "actor.h"
+
+
+// 上から落ちてくる足場となるレンガ
+class Brick : public Actor {
+private:
+  bool active_;
+
+public:
+  Brick();
+  Brick(const ofVec3f& pos, const ofVec3f& size, bool active);
+  
+  void setup() override;
+  void update(float deltaTime) override;
+  void draw() override;
+  
+  bool& active();
+};
