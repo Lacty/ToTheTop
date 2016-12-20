b5a1dcce5b0a032da13e51cfe8674f645c477f0e
Lacty <akira206@gmail.com>
2016-12-21 07:06:54 +0900

    BackGroundを追加

---
+++ src/BackGround/backGround.cpp
@@ -0,0 +1,19 @@
+
+/**
+ * @file     backGround.cpp
+ * @brief    背景
+ *
+ * @author   y.akira
+ * @date     2016.12.21
+ */
+
+#include "precompiled.h"
+
+
+void BackGround::windowResizeCallback() {}
+
+void BackGround::setup() {}
+
+void BackGround::update() {}
+
+void BackGround::draw() {}
---
+++ src/BackGround/backGround.h
@@ -0,0 +1,30 @@
+
+/**
+ * @file     backGround.h
+ * @brief    背景
+ *
+ * @author   y.akira
+ * @date     2016.12.21
+ */
+
+#pragma once
+
+
+/**
+ * @brief 宇宙をテーマにした背景を描画するクラス
+ */
+class BackGround {
+private:
+  ofVec2f windowSize_;
+  
+  //! windowのサイズが変わった際に呼ばれる関数
+  void windowResizeCallback();
+  
+public:
+  BackGround() {}
+  ~BackGround() {}
+  
+  void setup();
+  void update();
+  void draw();
+};
