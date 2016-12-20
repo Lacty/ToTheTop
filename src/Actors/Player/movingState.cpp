0620e8c84ffbea7835837acb6b9e35a5a5d2119f
wem-wem <spidertargal@yahoo.co.jp>
2016-12-20 23:33:42 +0900

    indentした

--- src/Actors/Player/movingState.cpp
+++ src/Actors/Player/movingState.cpp
@@ -15,13 +15,11 @@ void MovingState::setup(Player* player) {}
 void MovingState::handleInput(Player* player, StateManager* stateMgr, ofxJoystick& input) {}
 
 void MovingState::update(float deltaTime, Player* player, ofxJoystick& input) {
-  // enableCollision()をしたActorが左側に無ければ移動
   if (input.isPushing(Input::Left)) {
     float newLocation = player->getPos().x - player->getVel().x;
     player->setPos(ofVec2f(newLocation, player->getPos().y));
   }
 
-  // enableCollision()をしたActorが右側に無ければ移動
   else if (input.isPushing(Input::Right)) {
     float newLocation = player->getPos().x + player->getVel().x;
     player->setPos(ofVec2f(newLocation, player->getPos().y));
