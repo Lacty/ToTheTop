634180fc112b2aff25f4bbef3fb7d01209eacd35
wem-wem <spidertargal@yahoo.co.jp>
2016-12-25 02:42:04 +0900

    左右からぶつかってもBrickを貫通しないように※バグあり
    しかしぶつかりながらのジャンプの挙動がおかしい。

--- src/Actors/Player/standingState.cpp
+++ src/Actors/Player/standingState.cpp
@@ -39,13 +39,35 @@ void StandingState::draw(Player* player) {
 }
 
 void StandingState::onCollision(Player* player, Actor* c_actor) {
-  auto p_pos = player->getPos();
-  auto c_pos = c_actor->getPos();
+  auto p_pos  = player->getPos();
+  auto p_vel  = player->getVel();
+  auto p_size = player->getSize();
+  auto c_pos  = c_actor->getPos();
+  auto c_size = c_actor->getSize();
 
   // Brickに上からぶつかったら加速度を０に(左右への移動量はそのまま)
   // Brickの上にPlayerの位置を修正
-  if (p_pos.y > c_pos.y) {
-    player->setVel(ofVec2f(player->getVel().x, 0.0f));
-    player->setPos(ofVec2f(p_pos.x, c_pos.y + c_actor->getSize().y));
+  if (p_pos.y > c_pos.y &&
+    p_pos.x < c_pos.x + c_size.x &&
+    p_pos.x + p_size.x > c_pos.x) {
+    player->setVel(ofVec2f(p_vel.x, 0.0f));
+    player->setPos(ofVec2f(p_pos.x, c_pos.y + c_size.y));
+  }
+
+
+  // プレイヤーの左辺がアクターの右辺より左側ならCollision
+  if (p_pos.x < c_pos.x + c_size.x &&
+    p_pos.x + p_size.x > c_pos.x + c_size.x &&
+    p_pos.y < c_pos.y + c_size.y / 2) {
+    player->setVel(ofVec2f(0.0f, p_vel.y));
+    player->setPos(ofVec2f(c_pos.x + c_size.x, p_pos.y));
+  }
+
+  // プレイヤーの右辺がアクターの左辺より右側ならCollision
+  if (p_pos.x + p_size.x > c_pos.x &&
+    p_pos.x < c_pos.x &&
+    p_pos.y < c_pos.y + c_size.y / 2) {
+    player->setVel(ofVec2f(0.0f, p_vel.y));
+    player->setPos(ofVec2f(c_pos.x - p_size.x, p_pos.y));
   }
 }
