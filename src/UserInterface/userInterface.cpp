011cdb482e2baf6e80d581547c6ab67db0cb0647
Lacty <akira206@gmail.com>
2016-12-25 12:18:36 +0900

    uiBase追加

--- src/UserInterface/userInterface.cpp
+++ src/UserInterface/userInterface.cpp
@@ -1,9 +1,55 @@
-//
-//  userInterface.cpp
-//  ToTheTop
-//
-//  Created by Yanai on 2016/12/25.
-//
-//
 
-#include "userInterface.hpp"
+/**
+ * @file     userInterface.cpp
+ * @brief    UI
+ *
+ * @author   y.akira
+ * @date     2016.12.21
+ */
+
+#include "precompiled.h"
+
+
+uiBase::uiBase()
+  : name_         ( "NoName" )
+  , tag_          ( -1       )
+  , isDead_       ( false    )
+  , enableUpdate_ ( false    )
+  , finishSetup_  ( false    )
+{}
+
+/**
+ * @brief 名前を設定する
+ * @param [in] name 登録する名前
+ */
+void uiBase::setName(const string &name) { name_ = name; }
+/**
+ * @brief タグを設定する
+ * @param [in] tag 設定するタグ
+ */
+void uiBase::setTag(const int tag)       { tag_  = tag;  }
+/**
+ * @brief 位置を設定する
+ * @param [in] pos 設定する位置
+ */
+void uiBase::setPos(const ofVec3f &pos)  { pos_  = pos;  }
+
+//! 名前を返します
+const string&  uiBase::getName()  const { return name_; }
+//! タグを返します
+const int      uiBase::getTag()   const { return tag_;  }
+//! 位置を返します
+const ofVec3f& uiBase::getPos()   const { return pos_;  }
+
+void uiBase::destroy() { isDead_ = true;  }
+bool uiBase::isDead()  { return isDead_;  }
+
+void uiBase::enableUpdate()  { enableUpdate_ = true;  }
+void uiBase::disableUpdate() { enableUpdate_ = false; }
+
+bool uiBase::hasSetup() {
+  if (finishSetup_) return true;
+  finishSetup_ = true;
+  return false;
+}
+bool uiBase::shouldUpdate()    { return enableUpdate_; }
--- src/UserInterface/userInterface.h
+++ src/UserInterface/userInterface.h
@@ -1,14 +1,52 @@
-//
-//  userInterface.hpp
-//  ToTheTop
-//
-//  Created by Yanai on 2016/12/25.
-//
-//
 
-#ifndef userInterface_hpp
-#define userInterface_hpp
+/**
+ * @file     userInterface.h
+ * @brief    UI
+ *
+ * @author   y.akira
+ * @date     2016.12.21
+ */
 
-#include <stdio.h>
+#pragma once
 
-#endif /* userInterface_hpp */
+
+/**
+ * @brief UI基底クラス
+ */
+class uiBase {
+private:
+  bool         finishSetup_;
+  bool         enableUpdate_;
+
+protected:
+  string       name_;   ///< 名前
+  int          tag_;    ///< タグ
+  bool         isDead_; ///< 生死
+  ofVec3f      pos_;    ///< 位置
+  
+public:
+  uiBase();
+  virtual ~uiBase() {}
+  
+  virtual void    setup() {}
+  virtual void    update(float deltaTime) {}
+  virtual void    draw() {}
+  
+  
+  void            setName (const string&  name);
+  void            setTag  (const int       tag);
+  void            setPos  (const ofVec3f&  pos);
+  
+  const string&   getName() const;
+  const int       getTag()  const;
+  const ofVec3f&  getPos()  const;
+  
+  void            destroy();          ///< この関数を呼べばuiManagerから消滅する
+  bool            isDead();           ///< このuiが死んでいるかどうか
+  
+  void            enableUpdate();     ///< updateを有効にする
+  void            disableUpdate();    ///< updateを無効にする
+  
+  bool            hasSetup();         ///< setupを実行し終わったか
+  bool            shouldUpdate();     ///< updateを実行させるか
+};
