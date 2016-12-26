4ffa078c5e4bcf045d2c913b1ac7b047b9c3338c
Naoto Fujiwara <asyura1327@yahoo.co.jp>
2016-12-26 14:49:20 +0900

    Brick イージングあり

--- src/Actors/Brick/brick.cpp
+++ src/Actors/Brick/brick.cpp
@@ -6,6 +6,10 @@ void Brick::moveTo(ofVec2f& pos) {
 	fallPoint_ = pos;
 }
 
+ofVec2f Brick::getFallPoint() {
+	return fallPoint_;
+}
+
 Brick::Brick() {
 	name_ = "Brick";
 	tag_ = 1;
@@ -14,11 +18,22 @@ Brick::Brick() {
 void Brick::setup() {
 	enableUpdate();
 	enableCollision();
+
+	fallSetup();
+}
+
+void Brick::fallSetup() {
+	vel_ = ofVec2f(pos_.x, 0.0f);
+	animPos_.animateFromTo(pos_.y, fallPoint_.y);
+	animPos_.setDuration(1);
+	AnimCurve curve = (AnimCurve)(VERY_LATE_LINEAR);
+	animPos_.setCurve(curve);
 }
 
 void Brick::update(float deltaTime) {
-	vel_ = (fallPoint_ - pos_) / 10;
-	pos_ += vel_;
+	animPos_.update(deltaTime * 5);
+	vel_.y = animPos_.val();
+	pos_ = vel_;
 }
 
 void Brick::draw() {
--- src/Actors/Brick/brick.h
+++ src/Actors/Brick/brick.h
@@ -4,8 +4,10 @@
 
 class Brick : public Actor {
 private:
+	ofxAnimatableFloat animPos_;
 	//落下地点
 	ofVec2f fallPoint_;
+
 public:
 	Brick();
 	void setup() override;
@@ -13,8 +15,10 @@ public:
 	void draw() override;
 	void onCollision(Actor* c_actor) override;
 
+	//イージングのsetup
 	void fallSetup();
-
+	
 	//落下地点を指定する
 	void moveTo(ofVec2f& pos);
+	ofVec2f getFallPoint();
 };
