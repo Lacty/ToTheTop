56233e090ccb18c0ba362fc0f2051e335fe42669
Lacty <akira206@gmail.com>
2016-12-28 22:07:29 +0900

    actorTags.h追加　アクターのタグを管理します

--- src/Actors/actor.cpp
+++ src/Actors/actor.cpp
@@ -24,13 +24,15 @@ uintmax_t Actor::generateUid() {
  * @brief アクターのコンストラクタ
  *        メンバ変数の初期化をしています
  */
-Actor::Actor() :
-  uid_(generateUid()),
-  name_("NoName"),
-  finishSetup_(false),
-  enableUpdate_(false),
-  enableCollision_(false),
-  isDead_(false) {}
+Actor::Actor()
+  : uid_            ( generateUid() )
+  , name_           ( "NoName"      )
+  , tag_            ( NONE          )
+  , finishSetup_    ( false         )
+  , enableUpdate_   ( false         )
+  , enableCollision_( false         )
+  , isDead_(false)
+{}
 
 // ---------------------------------------------------------------------
 //
