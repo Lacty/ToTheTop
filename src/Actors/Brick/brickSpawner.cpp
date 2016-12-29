8dcc7ee2ed8be252fc45f291da4d9b8112ad662d
Lacty <akira206@gmail.com>
2016-12-30 00:13:44 +0900

    brickManager.json追加

---
+++ src/Actors/Brick/brickSpawner.cpp
@@ -0,0 +1,9 @@
+//
+//  brickSpawner.cpp
+//  ToTheTop
+//
+//  Created by Yanai on 2016/12/30.
+//
+//
+
+#include "brickSpawner.hpp"
---
+++ src/Actors/Brick/brickSpawner.h
@@ -0,0 +1,14 @@
+//
+//  brickSpawner.hpp
+//  ToTheTop
+//
+//  Created by Yanai on 2016/12/30.
+//
+//
+
+#ifndef brickSpawner_hpp
+#define brickSpawner_hpp
+
+#include <stdio.h>
+
+#endif /* brickSpawner_hpp */
