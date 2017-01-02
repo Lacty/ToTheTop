dcbe40cc916ac85e2ce1bf1eabb9df4d83fad588
Lacty <akira206@gmail.com>
2017-01-02 17:57:37 +0900

    doxygen用コメント追加

--- src/gameLocal.h
+++ src/gameLocal.h
@@ -25,15 +25,16 @@ public:
   GameLocal();
   ~GameLocal() {}
 
-  void Setup();
+  void    Setup();                 ///< このクラスの初期化
 
-  int Width() const;
-  int Height() const;
+  int     Width() const;           ///< ウィンドウの横幅を返す
+  int     Height() const;          ///< ウィンドウの縦幅を返す
   
-  float LastFrame() const;
-  float FrameAcc() const;
+  float   LastFrame() const;       ///< 前のフレームから現在にかけてかかった時間を返す
+  float   FrameAcc() const;        ///< ゲーム内時間の加速度 デフォルト 1
   
-  void SetFrameAcc(float acc);
+  void    SetFrameAcc(float acc);  ///< ゲーム内時間の加速度を設定
 };
 
+//! GameLocalへのアクセスはこの変数を介す
 extern unique_ptr<GameLocal> g_local;
