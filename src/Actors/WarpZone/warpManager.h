ea1b2afd0355cc3b60d10dff2655bdb99d574b2f
Naoto Fujiwara <asyura1327@yahoo.co.jp>
2017-01-08 17:45:15 +0900

    warpManager作成

---
+++ src/Actors/WarpZone/warpManager.h
@@ -0,0 +1,27 @@
+
+/**
+* @file   warpManager.h
+* @brief  ワープゾーンマネージャ－
+*
+* @author f.naoto
+* @date   2017.1.8
+*/
+#pragma once
+
+
+//! @brief ワープゾーンマネージャークラス
+class WarpManager : public Actor {
+private:
+	shared_ptr<WarpZone> warpZone_;
+	shared_ptr<Actor> player_;
+	ofVec2f spawnPos_;
+	ofVec2f destPos_;
+	const float limitPos_ = 200;
+	const float spwPosXMin_ = 100;
+	const float spwPosXMax_ = g_local->Width() - 100;
+	void spawnWarp();
+public:
+	WarpManager();
+	void setup() override;
+	void update(float deltaTime) override;
+};
