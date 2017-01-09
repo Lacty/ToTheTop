7726a4a2435902c163230621a8e47e492407daa3
Naoto Fujiwara <asyura1327@yahoo.co.jp>
2017-01-09 22:08:50 +0900

    warpManager Guiを追加しました

--- src/Actors/WarpZone/warpManager.h
+++ src/Actors/WarpZone/warpManager.h
@@ -16,12 +16,11 @@ private:
 	shared_ptr<Actor> player_;
 	ofVec2f spawnPos_;
 	ofVec2f destPos_;
-	const float limitPos_ = g_local->Height();
-	const float spwPosXMin_ = 100;
-	const float spwPosXMax_ = g_local->Width() - 100;
+	ofVec2f warpSize_;
 	void spawnWarp();
 public:
 	WarpManager();
 	void setup() override;
 	void update(float deltaTime) override;
+	void gui() override;
 };
